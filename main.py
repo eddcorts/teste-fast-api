a0ee104a5fb29baf248ed889b4d5bd147369b660
eddcorts <edfmcortes@gmail.com>
2021-08-05 17:56:54 -0300

    Add mais detalhes do ip conectando (?)

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from typing import Optional
 from datetime import datetime as dt
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def main(request: Request):
+    ip = request.client.host
     time = dt.now()
     
     with open('conexoes.log', 'r+') as logs:
         for line in logs:
             pass
-        idx, _ = line.split(': ') if line.strip() else ('0', '')
+        idx, *_ = line.split(': ') if line.strip() else ('0', '')
         
-        logs.write(f"{int(idx)+1}: {time}\n")
+        content = f"{int(idx)+1}: {time}: {ip}\n"
+        
+        logs.write(content)
+        print(f"Novo Acesso: {content}")
         
     return {
         "eae": "gay",
@@ -25,4 +29,5 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
+    print(f"TEM ALGUEM AQUI!!!! item_id: {item_id} query: {q}")
     return {"item_id": item_id, "q": q}
